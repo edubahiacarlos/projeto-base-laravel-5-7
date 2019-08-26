eb706a6b7a43da5c57427f4e69ba19123aa7ab56
edubahiacarlos <edubahia.carlos@hotmail.com>
2019-08-26 11:37:39 -0300

    mundando retorno

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,7 +47,7 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         
-        //return redirect('/');    
-        return parent::render($request, $exception);
+        return redirect('/');    
+       // return parent::render($request, $exception);
     }
 }
