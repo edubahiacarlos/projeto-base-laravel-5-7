2826e37e3a5c9a079080d4626acc12fe20b9c3e9
edubahiacarlos <edubahia.carlos@hotmail.com>
2019-08-28 22:37:29 -0300

    nova versao do app

--- resources/views/abc.blade.php
+++ resources/views/abc.blade.php
@@ -14,22 +14,25 @@
   <app-root></app-root>
   <script type="text/javascript" src="js/11.94fa246b9e06ddd837ef.js"></script>
   <script type="text/javascript" src="js/2.d5685a75608601df64bc.js"></script>
-  <script type="text/javascript" src="js/3.5aae626257a92759e2de.js"></script>
-  <script type="text/javascript" src="js/7.cdabe35b26dff458a236.js"></script>
+  <script type="text/javascript" src="js/3.7e62bcdcd96a1ecd96e4.js"></script>
+  <script type="text/javascript" src="js/7.4101c000db7709147ca0.js"></script>
 
-  <script type="text/javascript" src="js/8.21d2925fe86f10240c72.js"></script>
+  <script type="text/javascript" src="js/8.3cf745adfdd364e86a78.js"></script>
   <script type="text/javascript" src="js/9.17ca9aace40833bc85fe.js"></script>
-  <script type="text/javascript" src="js/10.a71f1fd3a232039dda1e.js"></script>
+  <script type="text/javascript" src="js/10.43194ddd3bddf4c437f5.js"></script>
 
   
 
-  <script type="text/javascript" src="js/12.0b07abba54b1693785f0.js"></script>
+  <script type="text/javascript" src="js/12.cbceef93b35a5058ab25.js"></script>
   
-  <script type="text/javascript" src="js/runtime.2c65ff9b87ce05ed6d08.js"></script>
+  
+  <script type="text/javascript" src="js/runtime.398dfc4c0cf35ead7bc8.js"></script>
   <script type="text/javascript" src="js/polyfills.6648d2003fdcfdd70fa3.js"></script>
+  
+
   <script type="text/javascript" src="js/common.671f904e7972b0c4c67d.js"></script>
 
-  <script type="text/javascript" src="js/main.fc5332c6720053ffb301.js"></script>
+  <script type="text/javascript" src="js/main.37c0c959a9b1e691b1c8.js"></script>
     
 </body>
 </html>
